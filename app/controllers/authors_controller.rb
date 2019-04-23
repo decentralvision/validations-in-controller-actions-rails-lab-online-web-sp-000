47086aebdb7ca06ad8284fea0473a06a6b11bc70
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-23 04:44:21 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,7 +8,7 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
-    if @author.valid?
+    if @author.save
       @author = Author.create!(author_params)
       redirect_to author_path(@author)
     else
